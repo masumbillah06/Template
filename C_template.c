cc379a9cf94e6afa3f73179668354ea344368cb5
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2023-09-13 19:57:27 +0600

    Create C_template.c

---
+++ C_template.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<stdbool.h>
+
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define ll long long
+#define si(x) scanf("%d",&x)
+#define sl(x) scanf("%lld",&x)
+#define ss(s) scanf("%s",s)
+#define pi(x) printf("%d\n",x)
+#define pl(x) printf("%lld\n",x)
+#define ps(s) printf("%s\n",s)
+
+void solve(){
+    
+}
+
+int main(){
+    int t = 1;
+    si(t);
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
