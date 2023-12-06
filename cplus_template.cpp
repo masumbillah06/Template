d0e4eea1c903f474b5eaf2574ebf44d6c58931b1
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2023-12-06 16:49:03 +0600

    Update cplus_template.cpp

--- cplus_template.cpp
+++ cplus_template.cpp
@@ -1,16 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 #define ll long long
 #define mp make_pair
 #define all(x) x.begin(), x.end()
-#define revall(x) x.begin(), x.end(), greater<>()
-#define clr(x) memset(x, 0, sizeof(x))
-#define fo(i,a,n) for(int i=a;i<n;i++)
-typedef vector<int> vi;
+typedef vector<int>  vi;
 typedef vector<bool> vb;
 typedef vector<vi>  vvi;
-
 
 int main()
 {
