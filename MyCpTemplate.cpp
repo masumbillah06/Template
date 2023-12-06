f405285aa5c4e76d526f5158ae211208fe4a75bb
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2023-12-06 16:49:41 +0600

    Update MyCpTemplate.cpp

--- MyCpTemplate.cpp
+++ MyCpTemplate.cpp
@@ -1,25 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define fo(i,a,n) for(int i=a;i<n;i++)
 #define ll long long
 #define pb push_back
 #define mp make_pair
 #define ff first
 #define ss second
 #define all(x) x.begin(), x.end()
-#define revall(x) x.begin(), x.end(), greater<>()
-#define clr(x) memset(x, 0, sizeof(x))
+#define clr(x,a) memset(x, a, sizeof(x))
 typedef vector<int>     vi;
+typedef vector<vi>	   vvi;
 typedef vector<double>  vd;
 typedef vector<bool>    vb;
 typedef vector<ll>	   vll;
-typedef vector<vi>	   vvi;
-typedef vector<vll>	   vvll;
 const int mod = 1'000'000'007;
 
 void fastIO(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 }
 
 void solve(){
@@ -27,14 +23,9 @@ void solve(){
 }
 
 int main(){
-
     fastIO();
-
     int t = 1;
     cin >> t;
-    while (t--)
-    {
-        solve();
-    }
+    while (t--) solve();
     return 0;
 }
