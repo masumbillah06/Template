4e5e0280e45a858a4d24c0b802470428162a8201
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2024-02-09 02:48:58 +0600

    Update and rename MyCpTemplate.cpp to My_CP_Template.cpp

---
+++ My_CP_Template.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define fio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<pair<int,int>> vpii;
+
+void solve(){
+    
+}
+
+int main(){
+    fio;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int tt=1;
+    cin>>tt;
+    while(tt--) solve();
+    return 0;
+}
