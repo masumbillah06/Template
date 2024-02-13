d90a9ccba25012090ef358a6abfb442e21a76684
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2024-02-13 18:47:42 +0600

    Update My_CP_Template.cpp

--- My_CP_Template.cpp
+++ My_CP_Template.cpp
@@ -6,11 +6,9 @@ using namespace std;
 #define ff first
 #define ss second
 #define all(x) x.begin(), x.end()
-#define allr(x) x.rbegin(), x.rend()
 #define fio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 typedef vector<int> vi;
 typedef vector<ll> vll;
-typedef vector<bool> vb;
 typedef vector<vi> vvi;
 typedef vector<pair<int,int>> vpii;
 
