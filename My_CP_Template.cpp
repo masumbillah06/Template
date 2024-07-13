a8f86828984debd3cd5031e1fb0a2821a781f25e
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2024-07-14 05:53:59 +0600

    Update My_CP_Template.cpp

--- My_CP_Template.cpp
+++ My_CP_Template.cpp
@@ -5,24 +5,21 @@ using namespace std;
 #define yes cout<<"YES\n"
 #define no cout<<"NO\n"
 #define all(x) x.begin(), x.end()
-#define fio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<vi> vvi;
 typedef vector<pair<int,int>> vpii;
-
-void file_in_out(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-    #endif
-}
 
 void solve(){
     
 }
 
 int main(){
-    fio; file_in_out();
+    fastIO;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+    #endif
 
     int tcase=1;
     cin>>tcase;
