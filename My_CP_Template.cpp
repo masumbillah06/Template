52de4edf286be1b9a61efa7698d28befb2f336a2
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2024-11-30 22:55:25 +0600

    Update My_CP_Template.cpp

--- My_CP_Template.cpp
+++ My_CP_Template.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 #define ll long long
 #define endl '\n'
-#define yes cout<<"Yes\n"
-#define no cout<<"No\n"
 #define all(x) x.begin(), x.end()
 typedef vector<int> vi;
 typedef vector<pair<int,int>> vpii;
@@ -13,10 +11,11 @@ void solve(){
 }
 
 int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
     #endif
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
     int tc=1;
     cin>>tc;
     while(tc--) solve();
